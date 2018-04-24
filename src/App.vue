0834a24bb58ca67c9e9a2a12b2f667e12a1311bc
yndxz <2465969877@qq.com>
2018-04-25 00:29:52 +0800

    VUE 2.0

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <mp3-Player :url="url" :music-name="name"></mp3-Player>
+    </div>
+</template>
+
+<script>
+    import mp3Player from './components/mp3Player';
+    export default {
+        name: "app",
+        data(){
+            return{
+                url:'http://m10.music.126.net/20180424234854/979a47cdb1ec0bbd222ce59b24d0e00b/ymusic/d8ce/08c3/986c/844405c5672efe9b10076bab25d7bce2.mp3',
+                name:'好久不见'
+            }
+        },
+        components:{
+            'mp3-Player':mp3Player
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueResource from 'vue-resource'
-import statement from '../src/components/play'
-import  rxUtils from '../src/assets/js/rxUtils'
+import App from '../src/App';
+import rxUtils from '../src/assets/js/rxUtils';
+
 rxUtils.setRem(15);
-Vue.use(VueResource);
 new Vue({
-  el: '#app',
-  components: { statement }
-})
+    el: '#app',
+    components: { App },
+    template: '<App/>'
+});
 
